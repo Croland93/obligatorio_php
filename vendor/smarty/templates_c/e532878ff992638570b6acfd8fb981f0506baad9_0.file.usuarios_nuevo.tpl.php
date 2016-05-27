e07a85575686d090ffa8e240a82f3dbcc9dfda69
Croland93 <matiase1993@gmail.com>
2016-05-27 18:22:39 -0300

    Validando datos en crear usuario

--- vendor/smarty/templates_c/e532878ff992638570b6acfd8fb981f0506baad9_0.file.usuarios_nuevo.tpl.php
+++ vendor/smarty/templates_c/e532878ff992638570b6acfd8fb981f0506baad9_0.file.usuarios_nuevo.tpl.php
@@ -1,17 +1,17 @@
 <?php
-/* Smarty version 3.1.29, created on 2016-05-26 18:09:27
+/* Smarty version 3.1.29, created on 2016-05-27 20:40:54
   from "C:\wamp\www\obligatorio_php\vistas\usuarios_nuevo.tpl" */
 
 if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
   'has_nocache_code' => false,
   'version' => '3.1.29',
-  'unifunc' => 'content_57473bd79ab3d6_81317507',
+  'unifunc' => 'content_5748b0d6439833_39131871',
   'file_dependency' => 
   array (
     'e532878ff992638570b6acfd8fb981f0506baad9' => 
     array (
       0 => 'C:\\wamp\\www\\obligatorio_php\\vistas\\usuarios_nuevo.tpl',
-      1 => 1464286166,
+      1 => 1464381651,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl,
     'file:cabezal.tpl' => 1,
   ),
 ),false)) {
-function content_57473bd79ab3d6_81317507 ($_smarty_tpl) {
+function content_5748b0d6439833_39131871 ($_smarty_tpl) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -30,13 +30,44 @@ function content_57473bd79ab3d6_81317507 ($_smarty_tpl) {
 ">
     
     <title><?php echo $_smarty_tpl->tpl_vars['proyecto']->value;?>
-</title>
-
+ - Crear cuenta</title>
+    <link rel="stylesheet" href="css/validationEngine.jquery.css" type="text/css" media="screen" title="no title" charset="utf-8">
+    
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="css/dashboard.css" rel="stylesheet">
+
+    <!-- Javascript personales -->
+    <?php echo '<script'; ?>
+ src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js" type="text/javascript"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="js/jquery.validationEngine-en.js" type="text/javascript"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="js/jquery.validationEngine.js" type="text/javascript"><?php echo '</script'; ?>
+>
+
+    <?php echo '<script'; ?>
+>  
+    $(document).ready(function() {
+      // SUCCESS AJAX CALL, replace "success: false," by:     success : function() { callSuccessFunction() }, 
+      
+      $("#formID").validationEngine()
+    });
+    
+    // JUST AN EXAMPLE OF VALIDATIN CUSTOM FUNCTIONS : funcCall[validate2fields]
+    function validate2fields(){
+      if($("#nombre").val() =="" || $("#apellido").val() == ""){
+        return true;
+      }else{
+        return false;
+      }
+    }
+  <?php echo '</script'; ?>
+>
     
   </head>
 
@@ -53,31 +84,35 @@ function content_57473bd79ab3d6_81317507 ($_smarty_tpl) {
         <?php }?>
         <div class="col-sm-3  col-md-3"></div>
         <div class="col-sm-6  col-md-6">
-        <form method="POST">
+        <form id="formID" class="formular" method="POST" action="">
           <fieldset class="form-group">
             <label for="Nickname">Nickname</label>
-            <input type="text" id="nickname" name="nickname" class="form-control" placeholder="Nickname" value='' required>
+            <input type="text" id="nickname" name="nickname" class="form-control validate[required,custom[noSpecialCaracters],length[0,30]]" placeholder="Nickname" value=''>
           </fieldset>
           <fieldset class="form-group">
             <label for="Nombre">Nombre</label>
-            <input type="text" id="nombre" name="nombre" class="form-control" placeholder="Nombre" value='' required>
+            <input type="text" id="nombre" name="nombre" class="form-control validate[required,custom[onlyLetter],length[0,30]] text-input" placeholder="Nombre" value=''>
           </fieldset>
           <fieldset class="form-group">
             <label for="Apellido">Apellido</label>
-            <input type="text" id="apellido" name="apellido" class="form-control" placeholder="Apellido" value='' required>
+            <input type="text" id="apellido" name="apellido" class="form-control validate[required,custom[onlyLetter],length[0,30]] text-input" placeholder="Apellido" value=''>
           </fieldset>
           <fieldset class="form-group">
             <label for="Password">Contraseña</label>
-            <input type="password" id="pass" name="pass" minlength="6" class="form-control" placeholder="Contraseña" value='' required>
+            <input type="password" id="pass" name="pass" class="form-control validate[required,length[6,30]] text-input" placeholder="Contraseña" value=''>
           </fieldset><fieldset class="form-group">
             <label for="Password">Repetir contraseña</label>
-            <input type="password" id="repass" name="repass" minlength="6" class="form-control" placeholder="Repetir contraseña" value='' required>
+            <input type="password" id="repass" name="repass" class="form-control validate[required,confirm[password]] text-input" placeholder="Repetir contraseña" value=''>
           </fieldset>
           <fieldset class="form-group">
             <label for="Email">Email</label>
-            <input type="text" id="email" name="email" class="form-control" placeholder="Email" value='' required>
+            <input type="text" id="email" name="email" class="form-control validate[required,custom[email]] text-input" placeholder="Email" value=''>
           </fieldset>
-          <input type="submit" value="Comenzar ahora!" class="form-control btn btn-primary">
+          <fieldset class="form-group">
+            <label for="Remail">Confirmar email</label>
+            <input type="text" id="reemail" name="reemail" class="form-control validate[required,confirm[email]] text-input" placeholder="Confirmar email" value=''>
+          </fieldset>
+          <input type="submit" value="Comenzar ahora!" class="submit form-control btn btn-primary">
         </form>
         </div>
       </div>
